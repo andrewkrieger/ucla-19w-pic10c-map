3feea3f85af3061cc5e7d3d3cdbc74ca8c5ef32a
Andrew Krieger <krieger.1618@gmail.com>
2019-01-15 12:32:10 -0800

    Add basic color lookup
    For now, just load colors into a vector and do a linear search.

--- color.cpp
+++ color.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 
 struct Color {
   float r, g, b;
@@ -19,13 +20,32 @@ std::istream& operator>>(std::istream& is,
 }
 
 int main() {
+  // Goal: read the database, then ask user for color names.
+  // If the user enters a valid name, print the color.
+  // Continue til the user says quit.
+
+  // Read the database.
   std::ifstream f("colors.txt");
+  std::vector<std::string> names;
+  std::vector<Color> colors;
   while (true) {
     std::string name;
     Color color;
     f >> name >> color;
+    names.push_back(name);
+    colors.push_back(color);
     if (!f) break;
-    std::cout << "Read color '" << name << "' = " << color
-              << '\n';
+  }
+
+  while (true) {
+    std::string input;
+    std::cout << "Enter a color name, or type quit: ";
+    std::cin >> input;
+    if (input == "quit") break;
+    for (int i = 0; i < names.size(); i++) {
+      if (names[i] == input) {
+        std::cout << colors[i] << std::endl;
+      }
+    }
   }
 }
