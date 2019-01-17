fa310417aec210945d25d1028adb97aac562b6f3
Andrew Krieger <krieger.1618@gmail.com>
2019-01-17 11:28:37 -0800

    Lookup via find()
    The find() method lets us access the result via an iterator. In
    particular, it can indicate that the searched value isn't in the map,
    without throwing an exception. The code uses that to report missing
    values in a more user-friendly way.

--- color.cpp
+++ color.cpp
@@ -19,9 +19,32 @@ std::istream& operator>>(std::istream& is,
   return is >> c.r >> c.g >> c.b;
 }
 
-Color getColor(const std::map<std::string, Color>& colors,
-               const std::string& color) {
-  return colors.at(color);
+// If `color_name` is a recognized name in `colors`,
+// returns a pointer to that color. Else returns nullptr.
+//
+// Remark: In modern C++, we usually use value semantics
+// (i.e. return-by-value), references, or smart pointers
+// (which call `delete` for us; we'll discuss it later this
+// quarter) instead of plain C-style pointers. One
+// reasonable use case for pointers is situations where you
+// want to return a "maybe-reference", i.e., either a
+// reference to an existing object or null (meaning no such
+// object). That's why this function is returning a pointer.
+const Color* getColor(
+    const std::map<std::string, Color>& colors,
+    const std::string& color_name) {
+  auto iterator = colors.find(color_name);
+  if (iterator == colors.end()) {
+    // color_name wasn't found
+    return nullptr;
+  } else {
+    // color_name was found. `iterator` points to a
+    // key-value pair. Let's unpack that key-value pair:
+    const std::string& key = iterator->first;
+    const Color& value = iterator->second;
+    // And return by pointer, using the address-of operator.
+    return &value;
+  }
 }
 
 int main() {
@@ -45,7 +68,11 @@ int main() {
     std::cout << "Enter a color name, or type quit: ";
     std::cin >> input;
     if (input == "quit") break;
-    Color color = getColor(colors, input);
-    std::cout << color << std::endl;
+    const Color *color = getColor(colors, input);
+    if (color) {
+      std::cout << *color << std::endl;
+    } else {
+      std::cout << "Error: no such color" << std::endl;
+    }
   }
 }
